0e126dd577346491830f8551228cc7b8e2eb9713
Vijay <vbose@apple.com>
2020-11-24 21:15:34 -0800

    Added java jar

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import com.scoreunit.rfb.encoding.Encodings;
+import com.scoreunit.rfb.service.RFBService;
+
+/**
+ * Hello world!
+ * vnc://192.168.29.107:9010
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println("Hello world");
+//        final RFBService service;
+//        service = new RFBService(9010);
+//        service.setPreferredEncodings(new int[]{Encodings.ZLIB, Encodings.HEXTILE});
+//        service.start();
+
+
+    }
+}
